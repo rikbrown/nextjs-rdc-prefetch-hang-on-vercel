35d0bb3f8fd38e227e105f83ecd8b7a4edc398ad
Rik Brown <rik@rik.codes>
2025-10-21 08:55:32 +0100

    wip

--- app/[locale]/[commitId]/(route)/(layout)/dynamic-page-auto-prefetch/page.tsx
+++ app/[locale]/[commitId]/(route)/(layout)/dynamic-page-auto-prefetch/page.tsx
@@ -3,5 +3,5 @@ import { connection } from 'next/server'
 export default async function DynamicPage() {
   await connection()
   await new Promise(resolve => setTimeout(resolve, 1000))
-  return <div>Dynamic Page (full prefetched) loaded</div>
+  return <div>Dynamic (auto pre-fetched) Page loaded</div>
 }
--- app/[locale]/[commitId]/(route)/(layout)/layout.tsx
+++ app/[locale]/[commitId]/(route)/(layout)/layout.tsx
@@ -4,7 +4,12 @@ import { connection } from 'next/server'
 export default async function Layout({ children }: { children: React.ReactNode }) {
   await connection()
   return <div data-dynamic-nested-layout>
-    <header>Dynamic Nested Layout (<Link prefetch href="/en/123/static-page">link to static</Link> | <Link prefetch href="/en/123/dynamic-page">link to dynamic</Link>)</header>
+    <header>Dynamic Nested Layout (
+      <Link prefetch href="/en/123/static-page">link to static</Link> | 
+      <Link prefetch href="/en/123/dynamic-page">link to dynamic</Link>) |
+      <Link href="/en/123/dynamic-page-auto-prefetch">link to dynamic (auto pre-fetched)</Link>) |
+      <Link prefetch={false} href="/en/123/dynamic-page-no-prefetch">link to dynamic (not pre-fetched)</Link>)
+    </header>
     {children}
   </div>
 }
